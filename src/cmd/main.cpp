05589aba5a8b3ed11e075dd5a8928b22ec793b79
borisov-artem1 <artem.borisov2002@mail.ru>
2025-03-21 18:55:41 +0300

    add: some comments

--- src/cmd/main.cpp
+++ src/cmd/main.cpp
@@ -19,6 +19,29 @@ int main(int argc, char **argv) {
 
         ::testing::InitGoogleTest(&argc, argv);
         return RUN_ALL_TESTS();
+
+        // std::string Regex;
+        // std::getline(std::cin, Regex);
+        // automat::automat nfa = automat::algForAuto::buildNFAfromPostfixString(Regex);
+        // automat::DFA dfa = automat::algForAuto::buildDFAfromNFA(nfa);
+        // automat::DFA dfaMin = automat::algForAuto::minimizationDFA(dfa, Regex);
+        // automat::DFA dfaFromRGX = automat::DFABuilder::buildMinimalDFA(Regex);
+        // automat::NFA graph;
+        //
+        // std::string inputString;
+        // std::cin.clear();
+        //
+        // while (true)
+        // {
+        //     std::getline(std::cin, inputString);
+        //     if (inputString == "exit") break;
+        //     std::cout << (graph.acceptString(nfa, inputString) ? "Accepted NFA\n" : "Rejected NFA\n");
+        //     std::cout << (automat::algForAuto::acceptStringForDFA(dfa.start, inputString) ? "Accepted DFA\n" : "Rejected DFA\n");
+        //     std::cout << (automat::algForAuto::acceptStringForDFA(dfaMin.start, inputString) ? "Accepted MinDFA\n" : "Rejected MinDFA\n");
+        //     std::cout << (automat::algForAuto::acceptStringForDFA(dfaFromRGX.start, inputString) ? "Accepted DFAfromRGX\n" : "Rejected DFAfromRGX\n");
+        //
+        // }
+
          // std::string Regex;
          // std::cin >> Regex;
          // automat::automat nfa = automat::algForAuto::buildNFAfromPostfixString(Regex);
